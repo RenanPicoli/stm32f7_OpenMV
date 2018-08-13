37bee13cf9a0dc4d8dbd0b7f50a48863f059842f
RenanPicoli <renanps123@gmail.com>
2018-08-13 10:18:55 -0300

    escrevo nos registradores da câmera os valores dados pela application note (YCbCr, 15fps se XCLK=24MHz)

--- Src/ov7725.c
+++ Src/ov7725.c
@@ -17,98 +17,89 @@
 #include "omv_boardconfig.h"
 
 const uint8_t default_regs[][2] = {
-    {COM3,          0x00},//COM3_SWAP_YUV},
-    {COM7,          COM7_RES_QVGA | COM7_FMT_RGB565 | COM7_FMT_YUV},
+		//extraído de Application Note, para XCLK = 24MHz
+		//OV7720, YCbCr, VGA
+		//15fps at 24MHz input clock, 4x maximum gain
+		//2/27/2007
 
-    {COM4,          COM4_PLL_BYPASS|COM4_AEC_FULL},
-    {CLKRC,         0xC0}, /* Bypass pre-scalar */
+		{0x3d, 0x03},
+		{0x17, 0x22},
+		{0x18, 0xa4},
+		{0x19, 0x07},
+		{0x1a, 0xf0},
+		{0x32, 0x00},
+		{0x29, 0xa0},
+		{0x2c, 0xf0},
+		{0x2a, 0x00},
+		{0x11, 0x01},
+		{0x42, 0x7f},
+		{0x4d, 0x09},
+		{0x63, 0xe0},
+		{0x64, 0xff},
+		{0x65, 0x20},
+		{0x66, 0x00},
+		{0x67, 0x48},
+		{0x13, 0xf0},
+		{0x0d, 0x41}, // 0x51/0x61/0x71 for different AEC/AGC window
+		{0x0f, 0xc5},
+		{0x14, 0x11},
+		{0x22, 0x7f},
+		{0x23, 0x03},
+		{0x24, 0x40},
+		{0x25, 0x30},
+		{0x26, 0xa1},
+		{0x2b, 0x00},
+		{0x6b, 0xaa},
+		{0x13, 0xff},
+		{0x90, 0x05},
+		{0x91, 0x01},
+		{0x92, 0x03},
+		{0x93, 0x00},
+		{0x94, 0xb0},
+		{0x95, 0x9d},
+		{0x96, 0x13},
+		{0x97, 0x16},
+		{0x98, 0x7b},
+		{0x99, 0x91},
+		{0x9a, 0x1e},
+		{0x9b, 0x08},
+		{0x9c, 0x20},
+		{0x9e, 0x81},
+		{0xa6, 0x06},
+		//Gamma
+		{0x7e, 0x0c},
+		{0x7f, 0x16},
+		{0x80, 0x2a},
+		{0x81, 0x4e},
+		{0x82, 0x61},
+		{0x83, 0x6f},
+		{0x84, 0x7b},
+		{0x85, 0x86},
+		{0x86, 0x8e},
+		{0x87, 0x97},
+		{0x88, 0xa4},
+		{0x89, 0xaf},
+		{0x8a, 0xc5},
+		{0x8b, 0xd7},
+		{0x8c, 0xe8},
+		{0x8d, 0x20},
+		// for 30 fps, 60Hz
+		{0x33, 0x00},
+		{0x22, 0x7f},
+		{0x23, 0x03},
+		// for 25 fps, 50Hz
+		//write_SCCB(0x33, 0x66);
+		//},0x22, 0x99);
 
-    // VGA Window Size
-    {HSTART,        0x3F},
-    {HSIZE,         0x50},
-    {VSTART,        0x03},
-    {VSIZE,         0x78},
-    {HREF,          0x00},
-
-    // Scale down to QVGA Resolution
-    {HOUTSIZE,      0x50},
-    {VOUTSIZE,      0x78},
-
-    {COM12,         0x03},
-    {EXHCH,         0x00},
-    {TGT_B,         0x7F},
-    {FIXGAIN,       0x09},
-    {AWB_CTRL0,     0xE0},
-    {DSP_CTRL1,     0xFF},
-
-    {DSP_CTRL2,     DSP_CTRL2_VDCW_EN | DSP_CTRL2_HDCW_EN | DSP_CTRL2_HZOOM_EN | DSP_CTRL2_VZOOM_EN},
-
-    {DSP_CTRL3,     0x00},
-    {DSP_CTRL4,     0x00},
-    {DSPAUTO,       0xFF},
-
-    {COM8,          0xF0},
-    {COM6,          0xC5},
-	{COM10,			COM10_HSYNC_EN},
-    {COM9,          0x21},
-    {BDBASE,        0x7F},
-    {BDSTEP,        0x03},
-    {AEW,           0x96},
-    {AEB,           0x64},
-    {VPT,           0xA1},
-    {EXHCL,         0x00},
-    {AWB_CTRL3,     0xAA},
-    {COM8,          0xFF},
-
-    //Gamma
-    {GAM1,          0x0C},
-    {GAM2,          0x16},
-    {GAM3,          0x2A},
-    {GAM4,          0x4E},
-    {GAM5,          0x61},
-    {GAM6,          0x6F},
-    {GAM7,          0x7B},
-    {GAM8,          0x86},
-    {GAM9,          0x8E},
-    {GAM10,         0x97},
-    {GAM11,         0xA4},
-    {GAM12,         0xAF},
-    {GAM13,         0xC5},
-    {GAM14,         0xD7},
-    {GAM15,         0xE8},
-
-    {SLOP,          0x20},
-    {EDGE1,         0x05},
-    {EDGE2,         0x03},
-    {EDGE3,         0x00},
-    {DNSOFF,        0x01},
-
-    {MTX1,          0xB0},
-    {MTX2,          0x9D},
-    {MTX3,          0x13},
-    {MTX4,          0x16},
-    {MTX5,          0x7B},
-    {MTX6,          0x91},
-    {MTX_CTRL,      0x1E},
-
-    {BRIGHTNESS,    0x08},
-    {CONTRAST,      0x20},
-    {UVADJ0,        0x81},
-    {SDE,           (SDE_CONT_BRIGHT_EN | SDE_SATURATION_EN)},
-
-    // For 30 fps/60Hz
-    {DM_LNL,        0x00},
-    {DM_LNH,        0x00},
-    {BDBASE,        0x7F},
-    {BDSTEP,        0x03},
-
-    // Lens Correction, should be tuned with real camera module
-    {LC_CTR,        0x01}, // Enable LC and use 1 coefficient for all 3 channels
-    {LC_RADI,       0x30}, // The radius of the circle where no compensation applies
-    {LC_COEF,       0x30}, // RGB Lens correction coefficient
-
-    // Frame reduction in night mode.
-    {COM5,          0xD5},
+		// Frame reduction in night mode.
+		//write_SCCB(0x23, 0x03);
+		// Lens Correction, should be tuned with real camera module
+		{0x4a, 0x10},
+		{0x49, 0x10},
+		{0x4b, 0x14},
+		{0x4c, 0x17},
+		{0x46, 0x05},
+		{0x0e, 0x65},
 
     {0x00,          0x00},
 };
